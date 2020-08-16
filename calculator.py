f85b6e2ae193d6a11adf5c8675719f0c5bd587d2
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-08-16 15:02:58 +0300

    calculator created

--- calculator.py
+++ calculator.py
@@ -1,8 +1,32 @@
 
 def main():
 	#write your code here
-	pass
-	
+	number1 = input("Enter the first number: ")
+	number2 = input("Enter the second number: ")
+
+	results = 0
+
+	if number1.isdigit() == False or number2.isdigit() == False:
+		print("The numbers were invalid")
+	else:
+		operation = input("Choose the operation (+, -, /, *): ")
+
+		if operation == "+":
+			results = int(number1) + int(number2)
+		elif operation == "-":
+			results = int(number1) - int(number2)
+		elif operation == "/":
+			results = int(number1) / int(number2)
+		elif operation == "*":
+			results = int(number1) * int(number2)
+		else:
+			print("The operation is not valid")
+
+
+	print("The answer is %d" % (results))
+
+
+
 
 
 
